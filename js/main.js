0aa1082107eaa96c51cb8c0da133d10cc2c8b067
David Ochoa <david.hernandez.ochoa.cbtis16@gmail.com>
2019-05-29 10:02:30 -0500

    9

--- js/main.js
+++ js/main.js
@@ -2,36 +2,38 @@ var audio;
 
 //Hide Pause Initially
 $('#pause').hide();
-
-//Initializer-Play firs Song
+	
+//Initializer - Play First Song
 initAudio($('#playlist li:first-child'));
-
+	
 function initAudio(element){
-	var song=element.attr('song');
-	var title=element.text();
-	var cover=element.attr('cover');
-	var artist=element.attr('artist');
+	var song = element.attr('song');
+    var title = element.text();
+    var cover = element.attr('cover');
+    var artist = element.attr('artist');
+
+	//Create a New Audio Object
+	audio = new Audio('media/' + song);
 	
-	//create a New audio Object
-	audio=new Audio('media/' + song);
-	
-	if (!audio.currentTime){
+	if(!audio.currentTime){
 		$('#duration').html('0.00');
 	}
-	$('#audio-player.title').text(title);
-	$('#audio-player.artist').text(artist);
+
+	$('#audio-player .title').text(title);
+    $('#audio-player .artist').text(artist);
 	
 	//Insert Cover Image
 	$('img.cover').attr('src','images/covers/' + cover);
 	
 	$('#playlist li').removeClass('active');
-	element.addClass('active');
+    element.addClass('active');
 }
 
+
 //Play Button
-$('#play').click(function(){
+$('#play').click(function (){
 	audio.play();
-	$('#play').hide(),
+	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
 	showDuration();
@@ -40,47 +42,48 @@ $('#play').click(function(){
 //Pause Button
 $('#pause').click(function(){
 	audio.pause();
-	$('#play').show(),
 	$('#pause').hide();
+	$('#play').show();
 });
-
+	
 //Stop Button
 $('#stop').click(function(){
-	audio.pause();
-	audio.currentTime=0;
+	audio.pause();		
+	audio.currentTime = 0;
 	$('#pause').hide();
-	$('#play').show(),
+	$('#play').show();
 	$('#duration').fadeOut(400);
 });
 
 //Next Button
 $('#next').click(function(){
-	audio.pause();
-	var next=$('#playlist li.active').next();
-	if (next.length==0){
-		next=$('#playlist li:first-child');
-	}
-	iniAudio(next);
+    audio.pause();
+    var next = $('#playlist li.active').next();
+    if (next.length = 0) {
+        next = $('#playlist li:first-child');
+    }
+    initAudio(next);
 	audio.play();
 	showDuration();
 });
+
 //Prev Button
 $('#prev').click(function(){
-	audio.pause();
-	var prev=$('#playlist li.active').prev();
-	if(prev.length==0){
-		prev=$('#playlist li:last-child');
-	}
-	iniAudio(prev);
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+    if (prev.length = 0) {
+        prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
 	audio.play();
 	showDuration();
 });
 
 //Playlist Song Click
-$('#playlist li').click(function() {
-	audio.pause();
-	initAudio($(this));
-	$('#play').hide(),
+$('#playlist li').click(function () {
+    audio.pause();
+    initAudio($(this));
+	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
 	audio.play();
@@ -89,23 +92,23 @@ $('#playlist li').click(function() {
 
 //Volume Control
 $('#volume').change(function(){
-	audio.volume=parseFloat(this.value/10);
+	audio.volume = parseFloat(this.value / 10);
 });
-
+	
 //Time Duration
 function showDuration(){
 	$(audio).bind('timeupdate', function(){
 		//Get hours and minutes
-		var s=parseInt(audio.currentTime%60);
-		var m=parseInt((audio.currentTime/60)%60);
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt((audio.currentTime / 60) % 60);
 		//Add 0 if seconds less than 10
-		if (s<10){
-			s='0' + s;
+		if (s < 10) {
+			s = '0' + s;
 		}
-		$('#duration').html(m+'.'+s);
-		var value=0;
-		if(audio.currentTime>0){
-			value=Math.floor((100/audio.duration)*audio.currentTime);
+		$('#duration').html(m + '.' + s);	
+		var value = 0;
+		if (audio.currentTime > 0) {
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
 		}
 		$('#progress').css('width',value+'%');
 	});
